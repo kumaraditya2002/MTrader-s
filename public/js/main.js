440d924760d5a588ee1d42346b6c3600fc9f8f48
Aditya Kumar <adityakumar9006@gmail.com>
2021-01-03 21:38:19 +0530

    sdvsdn

---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+let url2 = "http://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=15deb215901a48a3b66437aacca7219c";
+let mrq = document.getElementById("mq");
+fetch(url2).then(res => res.json())
+.then(data=>{
+    let hstr="";
+    data.articles.forEach(element => {
+        if(element.title.split('-').length==2)
+            hstr += `<a href="${element.url}" target=_blank id="mqc">${element.title.split('-')[0]}</a><img src="http://www.neccoal.co.in/images/New.gif" />`+`&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`;
+        
+    });
+    mrq.innerHTML=hstr;
+}).catch(err=>console.log(err));
